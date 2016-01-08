41b86456ffa9a454c8f567161ace4f46707c15cf
Livio Ribeiro <livioribeiro@outlook.com>
2016-01-08 16:21:31 -0300

    add map serialization

--- src/v1/packstream/serialize.rs
+++ src/v1/packstream/serialize.rs
@@ -300,20 +300,38 @@ impl<'a, W: Write> Encoder for PackstreamEncoder<'a, W> {
 
     fn emit_map<F>(&mut self, len: usize, f: F) -> Result<(), Self::Error>
         where F: FnOnce(&mut Self) -> Result<(), Self::Error> {
-        Ok(())
+
+            if len <= m::USE_TINY_MAP as usize {
+                try!(self.writer.write_u8(m::TINY_MAP_NIBBLE + len as u8));
+            } else if len <= m::USE_MAP_8 as usize {
+                try!(self.writer.write_u8(m::MAP_8));
+                try!(self.writer.write_u8(len as u8));
+            } else if len <= m::USE_MAP_16 as usize {
+                try!(self.writer.write_u8(m::MAP_16));
+                try!(self.writer.write_u16::<BigEndian>(len as u16));
+            } else if len <= m::USE_MAP_32 as usize {
+                try!(self.writer.write_u8(m::MAP_32));
+                try!(self.writer.write_u32::<BigEndian>(len as u32));
+            }
+
+            f(self)
     }
     fn emit_map_elt_key<F>(&mut self, idx: usize, f: F) -> Result<(), Self::Error>
         where F: FnOnce(&mut Self) -> Result<(), Self::Error> {
-        Ok(())
+
+        f(self)
     }
     fn emit_map_elt_val<F>(&mut self, idx: usize, f: F) -> Result<(), Self::Error>
         where F: FnOnce(&mut Self) -> Result<(), Self::Error> {
-        Ok(())
+
+        f(self)
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::collections::BTreeMap;
+    use std::string::String;
     use super::encode;
     use ::v1::packstream::marker as m;
 
@@ -616,6 +634,184 @@ mod tests {
                             0x01,
                             m::FLOAT, 0x3F, 0xF1, 0x99, 0x99, 0x99, 0x99, 0x99, 0x9A,
                             m::TINY_STRING_NIBBLE + 1, 0x41];
+
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn serialize_map32() {
+        let size = 70_000;
+        let input = (0..size).fold(
+            BTreeMap::<String, u32>::new(),
+            |mut acc, i| { acc.insert(format!("A{}", i), 1); acc }
+        );
+
+        let result = encode(&input).unwrap();
+        let expected = input.keys().fold(
+            vec![m::MAP_32, 0x00, 0x01, 0x11, 0x70],
+            |mut acc, i| {
+                acc.append(&mut encode(&i).unwrap());
+                acc.push(0x01);
+                acc
+            }
+        );
+
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn serialize_map16() {
+        let size = 5_000;
+        let input = (0..size).fold(
+            BTreeMap::<String, u32>::new(),
+            |mut acc, i| { acc.insert(format!("A{}", i), 1); acc }
+        );
+
+        let result = encode(&input).unwrap();
+        let expected = input.keys().fold(
+            vec![m::MAP_16, 0x13, 0x88],
+            |mut acc, i| {
+                acc.append(&mut encode(&i).unwrap());
+                acc.push(0x01);
+                acc
+            }
+        );
+
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn serialize_map8() {
+        let size = 200;
+        let input = (0..size).fold(
+            BTreeMap::<String, u32>::new(),
+            |mut acc, i| { acc.insert(format!("A{}", i), 1); acc }
+        );
+
+        let result = encode(&input).unwrap();
+        let expected = input.keys().fold(
+            vec![m::MAP_8, 0xC8],
+            |mut acc, i| {
+                acc.append(&mut encode(&i).unwrap());
+                acc.push(0x01);
+                acc
+            }
+        );
+
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn serialize_tiny_map() {
+        let size = 3;
+        let input = (0..size).fold(
+            BTreeMap::<String, u32>::new(),
+            |mut acc, i| { acc.insert(format!("A{}", i), 1); acc }
+        );
+
+        let result = encode(&input).unwrap();
+        let expected = input.keys().fold(
+            vec![m::TINY_MAP_NIBBLE + size],
+            |mut acc, i| {
+                acc.append(&mut encode(&i).unwrap());
+                acc.push(0x01);
+                acc
+            }
+        );
+
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn serialize_map_of_string() {
+        let size = 3;
+        let input = {
+            let mut input: BTreeMap<&'static str, &'static str> = BTreeMap::new();
+            input.insert("A", "abcdefghijklmnopqrstuvwxyz");
+            input.insert("B", "abcdefghijklmnopqrstuvwxyz");
+            input.insert("C", "abcdefghijklmnopqrstuvwxyz");
+            input
+        };
+
+        let result = encode(&input).unwrap();
+        let expected = vec![m::TINY_MAP_NIBBLE + size,
+                            0x81, 0x41,
+                            m::STRING_8, 0x1A, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66,
+                            0x67, 0x68, 0x69, 0x6A, 0x6B, 0x6C, 0x6D, 0x6E,
+                            0x6F, 0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76,
+                            0x77, 0x78, 0x79, 0x7A,
+                            0x81, 0x42,
+                            m::STRING_8, 0x1A, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66,
+                            0x67, 0x68, 0x69, 0x6A, 0x6B, 0x6C, 0x6D, 0x6E,
+                            0x6F, 0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76,
+                            0x77, 0x78, 0x79, 0x7A,
+                            0x81, 0x43,
+                            m::STRING_8, 0x1A, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66,
+                            0x67, 0x68, 0x69, 0x6A, 0x6B, 0x6C, 0x6D, 0x6E,
+                            0x6F, 0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76,
+                            0x77, 0x78, 0x79, 0x7A];
+
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn serialize_map_of_int() {
+        let size = 3;
+        let input = {
+            let mut input: BTreeMap<&'static str, u32> = BTreeMap::new();
+            input.insert("A", 32_000);
+            input.insert("B", 32_000);
+            input.insert("C", 32_000);
+            input
+        };
+
+        let result = encode(&input).unwrap();
+        let expected = vec![m::TINY_MAP_NIBBLE + size,
+                            0x81, 0x41, m::INT_16, 0x7D, 0x00,
+                            0x81, 0x42, m::INT_16, 0x7D, 0x00,
+                            0x81, 0x43, m::INT_16, 0x7D, 0x00];
+
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn serialize_map_of_float() {
+        let size = 3;
+        let input = {
+            let mut input: BTreeMap<&'static str, f64> = BTreeMap::new();
+            input.insert("A", 1.1);
+            input.insert("B", 1.1);
+            input.insert("C", 1.1);
+            input
+        };
+
+        let result = encode(&input).unwrap();
+        let expected = vec![m::TINY_MAP_NIBBLE + size,
+                            0x81, 0x41, m::FLOAT, 0x3F, 0xF1, 0x99, 0x99, 0x99, 0x99, 0x99, 0x9A,
+                            0x81, 0x42, m::FLOAT, 0x3F, 0xF1, 0x99, 0x99, 0x99, 0x99, 0x99, 0x9A,
+                            0x81, 0x43, m::FLOAT, 0x3F, 0xF1, 0x99, 0x99, 0x99, 0x99, 0x99, 0x9A,];
+
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn serialize_map_of_bool() {
+        let size = 4;
+        let input = {
+            let mut input: BTreeMap<&'static str, bool> = BTreeMap::new();
+            input.insert("A", true);
+            input.insert("B", false);
+            input.insert("C", true);
+            input.insert("D", false);
+            input
+        };
+
+        let result = encode(&input).unwrap();
+        let expected = vec![m::TINY_MAP_NIBBLE + size,
+                            0x81, 0x41, m::TRUE,
+                            0x81, 0x42, m::FALSE,
+                            0x81, 0x43, m::TRUE,
+                            0x81, 0x44, m::FALSE];
 
         assert_eq!(expected, result);
     }
