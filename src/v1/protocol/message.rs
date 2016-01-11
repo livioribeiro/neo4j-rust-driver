4e65f4bf1cf4c6886bbf36b11a2f1a5185052ae0
Livio Ribeiro <livioribeiro@outlook.com>
2016-01-11 11:56:24 -0300

    add Run message serialization

--- src/v1/protocol/message.rs
+++ src/v1/protocol/message.rs
@@ -1,6 +1,10 @@
+use std::convert::Into;
 use rustc_serialize::{Encodable, Encoder};
 
+use ::v1::packstream::value::{Value, Map};
+
 const INIT_SIZE: usize = 1;
+const RUN_SIZE: usize = 2;
 
 pub struct Init {
     client_name: String,
@@ -17,13 +21,48 @@ impl Init {
 impl Encodable for Init {
     fn encode<S: Encoder>(&self, e: &mut S) -> Result<(), S::Error> {
         try!(e.emit_struct("__STRUCTURE__INIT", INIT_SIZE as usize, |_| Ok(())));
-        self.client_name.encode(e)
+        try!(self.client_name.encode(e));
+
+        Ok(())
+    }
+}
+
+pub struct Run {
+    statement: String,
+    parameters: Map,
+}
+
+impl Run {
+    pub fn new(statement: &str) -> Self {
+        Run {
+            statement: statement.to_owned(),
+            parameters: Map::new(),
+        }
+    }
+
+    pub fn add_param<T: Into<Value>>(&mut self, name: &str, value: T) {
+        self.parameters.insert(name.to_owned(), value.into());
+    }
+
+    pub fn with_param<T: Into<Value>>(mut self, name: &str, value: T) -> Self {
+        self.add_param(name, value);
+        self
+    }
+}
+
+impl Encodable for Run {
+    fn encode<S: Encoder>(&self, e: &mut S) -> Result<(), S::Error> {
+        try!(e.emit_struct("__STRUCTURE__RUN", RUN_SIZE, |_| Ok(())));
+        try!(self.statement.encode(e));
+        try!(self.parameters.encode(e));
+
+        Ok(())
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::Init;
+    use super::{Init, Run};
     use ::v1::packstream::serialize::encode;
 
     #[test]
@@ -35,6 +74,36 @@ mod tests {
                             0x79, 0x43, 0x6C, 0x69,
                             0x65, 0x6E, 0x74, 0x2F,
                             0x31, 0x2E, 0x30];
+
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn serialize_run() {
+        let input = Run::new("RETURN 1 AS num");
+
+        let result = encode(&input).unwrap();
+        let expected = vec![0xB2, 0x10, 0x8F, 0x52,
+                            0x45, 0x54, 0x55, 0x52,
+                            0x4E, 0x20, 0x31, 0x20,
+                            0x41, 0x53, 0x20, 0x6E,
+                            0x75, 0x6D, 0xA0];
+
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn serialize_run_with_parameters() {
+        let input = Run::new("CREATE (n {p: {v}})").with_param("v", 1);
+
+        let result = encode(&input).unwrap();
+        let expected = vec![0xB2, 0x10, 0xD0, 0x13,
+                            0x43, 0x52, 0x45, 0x41,
+                            0x54, 0x45, 0x20, 0x28,
+                            0x6E, 0x20, 0x7B, 0x70,
+                            0x3A, 0x20, 0x7B, 0x76,
+                            0x7D, 0x7D, 0x29,
+                            0xA1, 0x81, 0x76, 0x01];
 
         assert_eq!(expected, result);
     }
--- src/v1/protocol/mod.rs
+++ src/v1/protocol/mod.rs
@@ -1,10 +1,12 @@
 pub mod message;
 
 pub const INIT: u8 = 0x01;
+pub const RUN: u8 = 0x10;
 
 pub fn signature(name: &str) -> Option<u8> {
     let sig = match name {
         "INIT" => INIT,
+        "RUN" => RUN,
         _ => return None,
     };
 
